3e39686ca5f349f3ff780aacefc6b3ee49ff7ac7
준써크 <junsuk8960@naver.com>
2023-12-22 15:06:06 +0900

    feat: 음성인식 구현

--- app/src/main/java/com/hackerton/noahah/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/hackerton/noahah/presentation/ui/main/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import com.hackerton.noahah.data.model.SpeechErrorMessage
 import com.hackerton.noahah.data.model.SpeechMessage
 import com.hackerton.noahah.databinding.ActivityMainBinding
 import com.hackerton.noahah.presentation.base.BaseActivity
@@ -17,11 +20,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     private lateinit var tts: TextToSpeech
     private var isTTsReady = false
 
+    private lateinit var speechRecognizer: SpeechRecognizer
+    private lateinit var recognizerIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // TTS 초기화
         tts = TextToSpeech(this, this)
+
+        // STT 초기화
+        initializeSpeechRecognizer()
 
         binding.btnUploadPdf.setOnClickListener {
             startActivity(Intent(this,ServiceActivity::class.java))
@@ -41,8 +50,69 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     private fun speakOut(text: String) {
         if (isTTsReady) {
             tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "utteranceId_2")
-            // 음성 인식 기능 추가
+            // 음성인식 시작
+            startListening()
         }
+    }
+
+    private fun initializeSpeechRecognizer() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        }
+
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onResults(results: Bundle?) {
+                val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                if (matches != null) {
+                    processResults(matches[0])
+                }
+            }
+
+            override fun onReadyForSpeech(params: Bundle?) {}
+            override fun onBeginningOfSpeech() {}
+            override fun onRmsChanged(rmsdB: Float) {}
+            override fun onBufferReceived(buffer: ByteArray?) {}
+            override fun onEndOfSpeech() {}
+            override fun onError(error: Int) {
+                restartListeningService() // 에러 발생 시 다시 시작
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {}
+            override fun onEvent(eventType: Int, params: Bundle?) {}
+        })
+    }
+
+    private fun startListening() {
+        speechRecognizer.startListening(recognizerIntent)
+    }
+
+    private fun processResults(command: String) {
+        // 임의의 문자열 포함 여부 확인
+        if (command.contains("노아", ignoreCase = true)) {
+            goToNextActivity()
+        } else {
+            // 메시지 출력 후 다시 듣기
+            showMessage(SpeechErrorMessage.NOT_EXIST_FILE_NAME.message)
+            restartListeningService()
+        }
+    }
+
+    private fun goToNextActivity() {
+        val intent = Intent(this, ServiceActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun showMessage(message: String) {
+        tts.speak(message, TextToSpeech.QUEUE_FLUSH, null, "utteranceId_4")
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun restartListeningService() {
+        speechRecognizer.stopListening()
+        speechRecognizer.startListening(recognizerIntent)
     }
 
     override fun onDestroy() {
@@ -50,6 +120,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             tts.stop()
             tts.shutdown()
         }
+        speechRecognizer.destroy()
         super.onDestroy()
     }
 }
