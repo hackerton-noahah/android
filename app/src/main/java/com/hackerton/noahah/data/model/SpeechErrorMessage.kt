1f964896aebfd1631ffa968e6a2aaa95f5bb0990
준써크 <junsuk8960@naver.com>
2023-12-22 14:20:20 +0900

    feat: 음성 문구 enum class 생성

---
+++ app/src/main/java/com/hackerton/noahah/data/model/SpeechErrorMessage.kt
@@ -0,0 +1,6 @@
+package com.hackerton.noahah.data.model
+
+enum class SpeechErrorMessage(message: String) {
+    NOT_EXIST_FILE_NAME("존재하지 않는 파일명입니다. 다시 말씀해주세요"),
+    NOT_EXIST_MODE("존재하지 않는 모드입니다. 다시 말씀해주세요"),
+}
