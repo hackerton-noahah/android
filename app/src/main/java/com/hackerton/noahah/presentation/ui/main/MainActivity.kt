0b4a0b3fdf09a751fb328300abfd870354e8a970
박진성 <plashdof@gmail.com>
2023-12-22 15:05:03 +0900

    feat : pdf 선택해서 불러온뒤, multipart 변환.

--- app/src/main/java/com/hackerton/noahah/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/hackerton/noahah/presentation/ui/main/MainActivity.kt
@@ -2,17 +2,32 @@ package com.hackerton.noahah.presentation.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.hackerton.noahah.databinding.ActivityMainBinding
 import com.hackerton.noahah.presentation.base.BaseActivity
 import com.hackerton.noahah.presentation.ui.service.ServiceActivity
 
+
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.btnUploadPdf.setOnClickListener {
-            startActivity(Intent(this,ServiceActivity::class.java))
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "application/pdf"
+            requestPDF.launch(intent)
         }
     }
+
+    private val requestPDF =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            it.data?.data?.let { uri->
+                val intent = Intent(this, ServiceActivity::class.java)
+                    .putExtra("pdfUri", uri.toString())
+                startActivity(intent)
+            }
+        }
+
 }
--- app/src/main/java/com/hackerton/noahah/presentation/ui/service/ServiceActivity.kt
+++ app/src/main/java/com/hackerton/noahah/presentation/ui/service/ServiceActivity.kt
@@ -1,9 +1,25 @@
 package com.hackerton.noahah.presentation.ui.service
 
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.net.toUri
 import com.hackerton.noahah.databinding.ActivityServiceBinding
 import com.hackerton.noahah.presentation.base.BaseActivity
+import com.hackerton.noahah.presentation.ui.toMultiPart
 
 class ServiceActivity: BaseActivity<ActivityServiceBinding>(ActivityServiceBinding::inflate) {
 
+    private val viewModel: ServiceViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if(intent.hasExtra("pdfUri")){
+            intent.getStringExtra("pdfUri")?.let{ pdfUri ->
+                val pdfMultiPart = pdfUri.toUri().toMultiPart(this)
+                viewModel.setMultiPart(pdfMultiPart)
+            }
+        }
+    }
 
 }
